921846a4331e92dfe533f7459a975916a1cba842
Trim21 <trim21me@gmail.com>
2017-07-08 23:38:28 +0800

    bug fix

--- app/main.py
+++ app/main.py
@@ -11,18 +11,26 @@ def manyUser():
         auth = request.args.get('auth')
         auth = base64.b64decode(auth).decode()
         auth = json.loads(auth)
+        print(auth)
         username = auth['username']
         password = auth['password']
     except:
-        return json.dumps({'error': 'username or password required'}), 401
+        resp = make_response(json.dumps(
+            {'error': 'username or password error'}))
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 401
     try:
         s = SduBkjws(username, password)
     except:
-        return json.dumps({'error': 'username or password error'}), 401
+        resp = make_response(json.dumps(
+            {'error': 'username or password error'}))
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp, 401
     s = json.dumps(s.get_now_score(), ensure_ascii=False,
                    indent='  ', sort_keys=True)
     resp = make_response(s)
     resp.headers['Content-Type'] = "application/json;charset=UTF-8"
+    resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
 
