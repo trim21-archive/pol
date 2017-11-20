73e2788a6acebd537c50f6f783ed7b568a6e143a
Trim21 <trim21me@gmail.com>
2017-11-20 19:01:31 +0800

    remove db

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_peewee.db import Database
+# from flask_peewee.db import Database
 
 app = Flask(__name__)
 app.config.from_object('config.Configuration')
-db = Database(app)
+# db = Database(app)
