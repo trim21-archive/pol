e479906e554e396888bb951612e9cec31341d442
Trim21 <trim21me@gmail.com>
2019-08-01 13:41:43 +0800

    update circleci config file

---
+++ .circleci/hash_requirement_files.py
@@ -0,0 +1,22 @@
+import os
+import hashlib
+from os import path
+from pathlib import Path
+
+project_root = Path(__file__) / '..' / '..'
+additional_requirements_path = [
+    '.pre-commit-config.yaml',
+]
+
+for dir, _, files in os.walk(path.join(project_root, 'requirements')):
+    for file in files:
+        file_path = path.normpath(path.join(dir, file))
+        if 'requirements' in file_path:
+            additional_requirements_path.append(file_path)
+
+m = hashlib.md5()
+for file in sorted(additional_requirements_path):
+    with open(file, 'rb') as f:
+        content = f.read()
+        m.update(content)
+print(m.hexdigest())
