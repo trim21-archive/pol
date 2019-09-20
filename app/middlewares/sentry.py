0fedd620bbb95ff752f95add75c67c8ced5f92a5
Trim21 <trim21me@gmail.com>
2019-09-21 01:19:59 +0800

    Update sentry.py

--- app/middlewares/sentry.py
+++ app/middlewares/sentry.py
@@ -10,16 +10,15 @@ class SentryMiddleware(Middleware):
         super().__init__(app)
 
     async def __call__(self, scope, receive=None, send=None):
-        async def __call__(self, scope, receive, send):
-            hub = sentry_sdk.Hub.current
-            with sentry_sdk.Hub(hub) as hub:
-                with hub.configure_scope() as sentry_scope:
-                    processor = functools.partial(
-                        self.event_processor, asgi_scope=scope
-                    )
-                    sentry_scope.add_event_processor(processor)
-                    try:
-                        await self.app(scope, receive, send)
-                    except Exception as exc:
-                        hub.capture_exception(exc)
-                        raise exc from None
+        hub = sentry_sdk.Hub.current
+        with sentry_sdk.Hub(hub) as hub:
+            with hub.configure_scope() as sentry_scope:
+                processor = functools.partial(
+                    self.event_processor, asgi_scope=scope
+                )
+                sentry_scope.add_event_processor(processor)
+                try:
+                    await self.app(scope, receive, send)
+                except Exception as exc:
+                    hub.capture_exception(exc)
+                    raise exc from None
