b118aaa94be99b230b94debea2f0a1a29c48248a
Trim21 <i@trim21.me>
2019-09-30 23:00:06 +0800

    build(deps): export hash from lock file

--- .circleci/export_requirements.py
+++ .circleci/export_requirements.py
@@ -1,27 +1,35 @@
 import os
-import sys
+import argparse
 
 import toml
 
 
-def _poetry_lock_to_requirements_txt(start_dir):
+def _poetry_lock_to_requirements_txt(start_dir, options):
     with open(f'{start_dir}/poetry.lock') as f:
         lock = toml.load(f)
+    hashes = lock['metadata']['hashes']
+    s = ''
+    for package in lock['package']:
+        if not options.all and (
+            package.get('category') != 'main' or package.get('optional')
+        ):
+            continue
 
-    requirements = {
-        package['name']: package['version'] + '; ' +
-        package.get('marker', '') if 'marker' in package else package['version']
-        for package in lock['package']
-        if package.get('category') == 'main' and not package.get('optional')
-    }
-    requirements = sorted(requirements.items(), key=lambda x: x[0])
+        name = package['name']
+        version = package['version']
+        marker = f'; {package["marker"]}' if 'marker' in package else ''
 
-    with open(f'{start_dir}/requirements.txt', 'w') as f:
-        f.writelines(f'{name}=={version} \n' for name, version in sorted(requirements))
+        hash_args = ' '.join([f'--hash=sha256:{x}' for x in hashes.get(name, [])])
+
+        s += f'{name}=={version} {marker} {hash_args}\n'
+
+    with open(f'{start_dir}/requirements.txt', 'w', encoding='utf-8') as f:
+        f.write(s)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--all', default=False, action='store_true')
+    opt = parser.parse_args()
     workdir = os.getcwd()
-    if len(sys.argv) > 1:
-        workdir = sys.argv[1]
-    _poetry_lock_to_requirements_txt(workdir)
+    _poetry_lock_to_requirements_txt(workdir, opt)
