d5b32216e8d4f11d964f873fd4c9f174737ebaa4
Trim21 <i@trim21.me>
2019-10-17 11:00:57 +0800

    fix(server): fix yield style depends

--- app/depends.py
+++ app/depends.py
@@ -1,11 +1,8 @@
-from contextlib import asynccontextmanager
-
 import httpx
 
 from app.core import config
 
 
-@asynccontextmanager
 async def aio_http_client() -> httpx.AsyncClient:
     aio_client = httpx.AsyncClient(
         headers={'user-agent': config.REQUEST_SERVICE_USER_AGENT}
--- tests/app/test_depends.py
+++ tests/app/test_depends.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import mock
 import httpx
 import pytest
@@ -8,7 +10,9 @@ from app.depends import aio_http_client
 
 @pytest.mark.asyncio
 async def test_aio_http_client():
-    async with aio_http_client() as client:
+    # todo: need a way to test yield style depends without context manager
+    async_ctx_manager = asynccontextmanager(aio_http_client)
+    async with async_ctx_manager() as client:
         assert isinstance(client, httpx.AsyncClient)
 
     with mock.patch('httpx.AsyncClient') as mocker:
@@ -16,6 +20,6 @@ async def test_aio_http_client():
         close = asynctest.mock.CoroutineMock()
         m.close = close
         mocker.return_value = m
-        async with aio_http_client() as client:
+        async with async_ctx_manager() as client:
             assert client is m
         close.assert_awaited_once()
