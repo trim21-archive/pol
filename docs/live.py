586048f9516d78336b511fcc9eca0f119e3feab6
Trim21 <trim21me@gmail.com>
2019-12-30 21:37:35 +0800

    chore: remove deps-dev livereload

--- docs/live.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from livereload import Server, shell
-
-server = Server()
-server.watch('source/*.*', shell('sphinx-build -M html source build'))
-server.serve(root='build/html')
