d4d376c3efddb719eecf92ed0530c128ff5a6e73
Trim21 <i@trim21.me>
2020-01-14 07:00:11 +0800

    f

--- app/db_models/__init__.py
+++ app/db_models/__init__.py
@@ -1,4 +1,3 @@
-import json
 import datetime
 
 import peewee as pw
@@ -6,55 +5,6 @@ import peewee as pw
 from app.db_models.base import S
 from app.db_models.iqiyi import IqiyiBangumi, IqiyiEpisode
 from app.db_models.bilibili import BilibiliBangumi, BilibiliEpisode
-
-
-class MyJSONField(pw.TextField):
-    def python_value(self, value):
-        if value is not None:
-            try:
-                return json.loads(value)
-            except (TypeError, ValueError):
-                return value
-
-    def db_value(self, value):
-        if value is not None:
-            return json.dumps(value)
-
-
-class Subject(S.BgmIpViewer):
-    id = pw.IntegerField(primary_key=True, index=True)
-    name = pw.CharField()
-    image = pw.CharField()
-    subject_type = pw.CharField()
-    name_cn = pw.CharField()
-    locked = pw.BooleanField(default=False)
-
-    tags = pw.TextField()
-    info = MyJSONField()
-    score_details = MyJSONField()
-
-    score = pw.CharField()
-    wishes = pw.IntegerField(default=0)
-    done = pw.IntegerField(default=0)
-    doings = pw.IntegerField(default=0)
-    on_hold = pw.IntegerField(default=0)
-    dropped = pw.IntegerField(default=0)
-
-    map = pw.IntegerField(index=True, default=0)
-
-    def __repr__(self):
-        return f'<Subject id={self.id}, name={self.name}>'
-
-    __str__ = __repr__
-
-
-class Tag(S.BgmIpViewer):
-    class Meta:
-        primary_key = pw.CompositeKey('subject_id', 'text')
-
-    subject_id = pw.IntegerField()
-    text = pw.FixedCharField(max_length=32, index=True)
-    count = pw.IntegerField()
 
 
 class Ep(S.BgmIpViewer):
@@ -82,8 +32,6 @@ __all__ = [
     'IqiyiEpisode',
     'BilibiliBangumi',
     'BilibiliEpisode',
-    'Subject',
-    'Tag',
     'Ep',
 ]
 
