1de33d602a869fdf2b545d11185395c4c5c90422
Trim21 <i@trim21.me>
2020-01-14 07:00:41 +0800

    f

--- app/db_models/__init__.py
+++ app/db_models/__init__.py
@@ -1,5 +1,3 @@
-import datetime
-
 import peewee as pw
 
 from app.db_models.base import S
@@ -12,19 +10,6 @@ class Ep(S.BgmIpViewer):
     ep_id = pw.IntegerField(primary_key=True)
     name = pw.CharField(max_length=400)
     episode = pw.CharField()
-
-
-class UserSubmitEpisode(S.BgmIpViewer):
-    class Meta:
-        table_name = 'user_submit_episode'
-
-    source = pw.FixedCharField(max_length=40)
-    source_ep_id = pw.CharField()
-    bgm_ep_id = pw.IntegerField()
-    episode = pw.IntegerField()
-    user_id = pw.IntegerField()
-    create_time = pw.DateTimeField(default=datetime.datetime.now)
-    modify_time = pw.DateTimeField(default=datetime.datetime.now)
 
 
 __all__ = [
