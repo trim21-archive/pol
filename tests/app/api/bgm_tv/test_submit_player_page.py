e54dc707d043d8cbf65f709a6c355ac5d6e5c68f
Trim21 <i@trim21.me>
2020-05-08 23:19:34 +0800

    chore: update pre-commit

--- tests/app/api/bgm_tv/test_submit_player_page.py
+++ tests/app/api/bgm_tv/test_submit_player_page.py
@@ -1,4 +1,5 @@
-import mock
+from unittest import mock
+
 import pytest
 from starlette.testclient import TestClient
 
--- tests/etc/test_gunicorn_conf.py
+++ tests/etc/test_gunicorn_conf.py
@@ -1,6 +1,6 @@
 import os
+from unittest import mock
 
-import mock
 import pytest
 from etc.gunicorn import get_worker
 
