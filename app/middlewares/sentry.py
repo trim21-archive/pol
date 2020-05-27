f31495602121d67d740c29bb6823943e4ec9e0d4
Trim21 <i@trim21.me>
2020-05-28 04:52:56 +0800

    remove sentry integration

--- app/middlewares/sentry.py
+++ app/middlewares/sentry.py
@@ -1,7 +1,6 @@
 import sentry_sdk
 from fastapi import FastAPI
 from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
-from sentry_sdk.integrations.redis import RedisIntegration
 from sentry_sdk.integrations.logging import ignore_logger
 
 from app.core import config
@@ -10,7 +9,5 @@ from app.core import config
 def setup_sentry(app: FastAPI):
     if config.DSN:  # pragma: no cover
         ignore_logger("asyncio")
-        sentry_sdk.init(
-            dsn=config.DSN, release=config.COMMIT_SHA, integrations=[RedisIntegration()]
-        )
+        sentry_sdk.init(dsn=config.DSN, release=config.COMMIT_SHA)
         app.add_middleware(SentryAsgiMiddleware)
